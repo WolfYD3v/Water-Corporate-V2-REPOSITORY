ba4d2ef36bb814dcdd2a7f0d8991cefc64f1afa3
WolfY_D3v <larueulrick89@gmail.com>
2026-01-06 08:51:57 +0100

    Changed changing room system + added working Input mouse detectionin the system

--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -7,25 +7,36 @@ class_name Map
 var actual_room: BaseRoom = null:
 	set(value):
 		if actual_room:
-			actual_room.hide()
+			actual_room.active = false
 		actual_room = value
-		actual_room.show()
+		actual_room.active = true
 		player.position = actual_room.get_player_position_in_room()
 
 func _ready() -> void:
 	actual_room = rooms.get_child(0)
+	
+	for room: BaseRoom in rooms.get_children():
+		room.change_room.connect(try_change_room)
+		room.active = false
+	
+	actual_room.active = true
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
-		if Input.is_key_pressed(KEY_LEFT) and actual_room.check_if_adj_have_room(0):
-			actual_room = actual_room.west_room
+		if Input.is_key_pressed(KEY_LEFT):
+			try_change_room(actual_room.west_room_idx)
 			player.rotation.y = deg_to_rad(90.0)
-		if Input.is_key_pressed(KEY_RIGHT) and actual_room.check_if_adj_have_room(1):
-			actual_room = actual_room.east_room
+		if Input.is_key_pressed(KEY_RIGHT):
+			try_change_room(actual_room.east_room_idx)
 			player.rotation.y = deg_to_rad(-90.0)
-		if Input.is_key_pressed(KEY_UP) and actual_room.check_if_adj_have_room(2):
-			actual_room = actual_room.north_room
+		if Input.is_key_pressed(KEY_UP):
+			try_change_room(actual_room.north_room_idx)
 			player.rotation.y = deg_to_rad(0.0)
-		if Input.is_key_pressed(KEY_DOWN) and actual_room.check_if_adj_have_room(3):
-			actual_room = actual_room.south_room
+		if Input.is_key_pressed(KEY_DOWN):
+			try_change_room(actual_room.south_room_idx)
 			player.rotation.y = deg_to_rad(180.0)
+
+func try_change_room(next_room_direcion_idx: int) -> void:
+	next_room_direcion_idx = clampi(next_room_direcion_idx, 0, 3)
+	if actual_room.check_if_adj_have_room(next_room_direcion_idx):
+		actual_room = actual_room.adj_rooms_array[next_room_direcion_idx]
--- scenes/rooms/base_room.gd
+++ scenes/rooms/base_room.gd
@@ -1,28 +1,89 @@
 extends Node3D
 class_name BaseRoom
 
+signal change_room(next_room_direction_idx: int)
+
 @export var west_room: BaseRoom = null
 @export var east_room: BaseRoom = null
 @export var north_room: BaseRoom = null
 @export var south_room: BaseRoom = null
 
 @onready var player_position_marker: Marker3D = $PlayerPositionMarker
+@onready var change_room_areas: Node3D = $ChangeRoomAreas
 
 var adj_rooms_array: Array[BaseRoom] = []
 var adj_rooms_directions: Array[String] = ["west_room", "east_room", "north_room", "south_room"]
+var change_rooms_areas_activated: Array[Area3D] = []
+
+var west_room_idx: int = 0
+var east_room_idx: int = 1
+var north_room_idx: int = 2
+var south_room_idx: int = 3
+var next_room_direction_idx: int = -1
+
+var active: bool = false:
+	set(value):
+		active = value
+		visible = value
+		if value: process_mode = Node.PROCESS_MODE_INHERIT
+		else: process_mode = Node.PROCESS_MODE_DISABLED
+		for change_room_area: Area3D in change_rooms_areas_activated:
+			change_room_area.get_child(0).disabled = not(value)
 
 func _ready() -> void:
 	hide()
 	await get_tree().create_timer(0.5).timeout
 	scan_adj_rooms()
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and active:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			_send_change_room_area_trigger()
+
 func check_if_adj_have_room(idx: int) -> bool:
 	return adj_rooms_array[idx] != null
 
 func scan_adj_rooms() -> void:
+	var idx: int = 0
 	for adj_room_variable: String in adj_rooms_directions:
-		adj_rooms_array.append(self.get(adj_room_variable))
+		adj_rooms_array.append(get(adj_room_variable))
+		if get(adj_room_variable):
+			change_rooms_areas_activated.append(change_room_areas.get_child(idx))
+			change_room_areas.get_child(idx).get_child(0).disabled = false
+		else:
+			change_room_areas.get_child(idx).get_child(0).disabled = true
+		idx += 1
 	print(to_string() +  " adj rooms scanned | RESULT: " + str(adj_rooms_array))
 
 func get_player_position_in_room() -> Vector3:
 	return player_position_marker.global_position
+
+func _send_change_room_area_trigger() -> void:
+	if next_room_direction_idx >= 0:
+		change_room.emit(next_room_direction_idx)
+
+
+
+func _on_west_trigger_area_mouse_entered() -> void:
+	next_room_direction_idx = west_room_idx
+
+func _on_west_trigger_area_mouse_exited() -> void:
+	next_room_direction_idx = -1
+
+func _on_east_trigger_area_mouse_entered() -> void:
+	next_room_direction_idx = east_room_idx
+
+func _on_east_trigger_area_mouse_exited() -> void:
+	next_room_direction_idx = -1
+
+func _on_north_trigger_area_mouse_entered() -> void:
+	next_room_direction_idx = north_room_idx
+
+func _on_north_trigger_area_mouse_exited() -> void:
+	next_room_direction_idx = -1
+
+func _on_south_trigger_area_mouse_entered() -> void:
+	next_room_direction_idx = south_room_idx
+
+func _on_south_trigger_area_mouse_exited() -> void:
+	next_room_direction_idx = -1
